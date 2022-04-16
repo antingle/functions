cbb45c70baf452dbb75933babc9a15b26f1e3230
Anthony Ingle <ingle.anthony@icloud.com>
2022-04-16 01:49:30 -0400

    refactored buttons into a view

---
+++ Calculator/ButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonView.swift
+//  Calculator
+//
+//  Created by Anthony Ingle on 4/16/22.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonView()
+    }
+}
