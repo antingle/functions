bc18ceab67b5be998648c357e09413dc8dd02a94
Anthony Ingle <ingle.anthony@icloud.com>
2022-05-02 20:40:34 -0400

    HistoryView scrolls when cycling out of view

--- Calculator/Views/HistoryView.swift
+++ Calculator/Views/HistoryView.swift
@@ -15,46 +15,61 @@ struct HistoryView: View {
     var body: some View {
         // this is an upside down scroll view to show history of expressions and solutions
         ScrollView(.vertical, showsIndicators: false, content: {
-            LazyVStack(alignment: .center, spacing: nil, content: {
+            ScrollViewReader { value in
                 
-                // this is reversed order since it is flipped
-                ForEach(historyStore.history) { item in
-                    VStack {
-                        HStack {
-                            Text(item.solution)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                .font(.title2)
-                                .onTapGesture {
-                                    expression += item.solution
-                                }
-                                .foregroundColor(historyIndex != -1 ? (item.id == historyStore.history[historyIndex].id ? .accentColor : .primary) : .primary)
-                            
-                            // copy solution button
-                            Button {
-                                let pasteBoard = NSPasteboard.general
-                                pasteBoard.clearContents()
-                                pasteBoard.writeObjects([item.solution as NSString])
-                            } label: {
-                                Image(systemName: "doc.on.doc").foregroundColor(.secondary)
-                            }.buttonStyle(.plain)
-                            
-                        }
-                        .rotationEffect(Angle(degrees: 180)).scaleEffect(x: -1.0, y: 1.0, anchor: .center)
-                        .padding(.top, 2)
-                        Text(item.expression)
-                            .rotationEffect(Angle(degrees: 180)).scaleEffect(x: -1.0, y: 1.0, anchor: .center)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .font(.body)
-                            .foregroundColor(.gray)
-                            .onTapGesture {
-                                expression += item.expression
+                LazyVStack(alignment: .center, spacing: nil, content: {
+                    
+                    // this is reversed order since it is flipped
+                    ForEach(historyStore.history) { item in
+                        VStack {
+                            HStack {
+                                Text(item.solution)
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                    .font(.title2)
+                                    .onTapGesture {
+                                        expression += item.solution
+                                    }
+                                    .foregroundColor(historyIndex != -1 ? (item.id == historyStore.history[historyIndex].id ? .accentColor : .primary) : .primary)
+                                    .id(item.id)
+                                
+                                // copy solution button
+                                Button {
+                                    let pasteBoard = NSPasteboard.general
+                                    pasteBoard.clearContents()
+                                    pasteBoard.writeObjects([item.solution as NSString])
+                                } label: {
+                                    Image(systemName: "doc.on.doc").foregroundColor(.secondary)
+                                }.buttonStyle(.plain)
                                 
                             }
+                            .rotationEffect(Angle(degrees: 180)).scaleEffect(x: -1.0, y: 1.0, anchor: .center)
+                            .padding(.top, 2)
+                            Text(item.expression)
+                                .rotationEffect(Angle(degrees: 180)).scaleEffect(x: -1.0, y: 1.0, anchor: .center)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.body)
+                                .foregroundColor(.gray)
+                                .onTapGesture {
+                                    expression += item.expression
+                                    
+                                }
+                        }
+                        .padding(.bottom, 4)
+                        .onChange(of: historyIndex) { newIndex in
+                            withAnimation {
+                                if newIndex != -1 {
+                                    value.scrollTo(historyStore.history[newIndex].id)
+                                }
+                                else if !historyStore.history.isEmpty
+                                {
+                                    value.scrollTo(historyStore.history[0].id)
+                                }
+                            }
+                        }
                     }
-                    .padding(.bottom, 4)
-                }
-                
-            })
+                    
+                })
+            }
         }).rotationEffect(Angle(degrees: 180)).scaleEffect(x: -1.0, y: 1.0, anchor: .center)
     }
 }
